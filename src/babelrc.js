22ee99ffe932c1822890367255d31ea27b11cb5f
振震 <865702942@qq.com>
2016-08-10 22:51:29 +0800

    babel-preset-es2015-webpack to babel-preset-es2015 with modules

--- src/babelrc.js
+++ src/babelrc.js
@@ -5,7 +5,10 @@ import { tmpdir } from 'os';
 export default {
   cacheDirectory: tmpdir(),
   presets: [
-    require.resolve('babel-preset-es2015-webpack'),
+    [
+      require.resolve('babel-preset-es2015'),
+      { 'modules': false }
+    ],
     require.resolve('babel-preset-react'),
     require.resolve('babel-preset-stage-0')
   ],
