da18d35de528df23cab1985092b5b6edf3d2e681
振震 <865702942@qq.com>
2017-01-31 19:01:45 +0800

    webpack to 2.2.1

--- test/expect/missing/main.js
+++ test/expect/missing/main.js
@@ -70,7 +70,7 @@
 /* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./missing\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./missing\""); e.code = 'MODULE_NOT_FOUND';; throw e; }()));
 
 /***/ })
 /******/ ]);
